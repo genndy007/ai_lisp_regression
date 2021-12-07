aacb5e42ebd68a8ff1ebcebd9af8dd237df3da4d
genndy007 <kochevgenazp@gmail.com>
2021-12-07 13:21:12 +0200

    Task 1 with common lisp

---
+++ task1.lisp
@@ -0,0 +1,52 @@
+(setf x (make-array '(4 3)))
+(setf y (make-array '(4 3)))
+(setf z (make-array '(4 3)))
+
+
+(print "First array:")
+(terpri)
+
+;; fill one array
+(dotimes (i 4)
+    (dotimes (j 3)
+        (setf (aref x i j) (read))
+    )
+)
+
+(print "Second array:")
+(terpri)
+
+;;  fill second array
+(dotimes (i 4)
+    (dotimes (j 3)
+        (setf (aref y i j) (read))
+    )
+)
+
+
+(dotimes (i 4)
+    (dotimes (j 3)
+        (if (>= (aref x i j) (aref y i j)) 
+            (setf (aref z i j) (aref x i j))
+        )
+        (if (< (aref x i j) (aref y i j))
+            (setf (aref z i j) (aref y i j))
+        )
+    )
+)
+
+
+(defun print-2d-array-as-table (array)
+  (loop for i from 0 below (array-dimension array 0)
+        do (loop for j from 0 below (array-dimension array 1)
+                 do (princ (aref array i j))
+                    (if (= j (1- (array-dimension array 1)))
+                        (terpri)
+                        (princ #\Space)))))
+
+
+(print-2d-array-as-table z)
+
+
+
+
